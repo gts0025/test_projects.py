58181fb4571f550a5b9485165d89fabb20925ae6
guilherme <guilherme>
2024-04-20 22:30:58 -0300

    Update de_jong_atractor.py
    new way of visualiing de jung atractor using moving particcles

--- python/de_jong_atractor.py
+++ python/de_jong_atractor.py
@@ -1,32 +1,68 @@
 import pygame
 from math import sin,cos
+from random import randint
 size = (700,400)
 point = [0,0]
-a = -0.2
-b = 4
-c = 5.7
-d = 1.1
+a = -1.1
+b = 2
+c = -2.7
+d = -5
 clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode(size)
 wipe = pygame.Surface(size)
 wipe.set_alpha(100)
+points = []
+for i in range(1000):
+    points.append([randint(-100,100)/100,randint(-100,100)/100,0,0])
+    
+
+def mov_points(point):
+    global t
+    px = sin(a*point[1])-cos(b*point[0])
+    py = sin(c*point[0])-cos(d*point[1])
+    
+    if px != point[0]:
+        dx = (px-point[0])*0.001
+    
+    if py != point[0]:
+        dy = (py-point[1])*0.001
+        
+    
+    
+    point[2] += dx
+    point[3] += dy
+    
+    point[0] += point[2]
+    point[1] += point[3]
+    
+    point[2] -= point[2]*0.01
+    point[3] -= point[3]*0.01
+    
+def simple_dejung(t):
+        for i in range(t):
+            pygame.draw.circle(screen,"white",(350+round((point[0])*150),200+round((point[1])*100)),1)
+            px = sin(a*point[1])-cos(b*point[0])
+            py = sin(c*point[0])-cos(d*point[1])
+            point[0] = px
+            point[1] = py
+
+def particles_dejung():
+    for i in points:
+        pygame.draw.circle(screen,"white",(350+round((i[0])*150),200+round((i[1])*100)),1)
+        mov_points(i)
+        
 t = 0
 while True:
     for event in pygame.event.get():
-        pass
-    
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
     screen.blit(wipe,(0,0))
-    for i in range(20000):
-        pygame.draw.circle(screen,"white",(350+round((point[0])*150),200+round((point[1])*100)),1)
-        px = sin(a*point[1])-cos(b*point[0])
-        py = sin(c*point[0])-cos(d*point[1])
-        point[0] = px
-        point[1] = py
-    
     point = [0,0]
-    a += 0.001
-        
+    
+    particles_dejung()
+    #simple_dejung(1000)
     #print(point[0],point[1])
     pygame.display.flip()    
     clock.tick(60)
