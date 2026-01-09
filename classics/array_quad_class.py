05cc6ed5e623a834cba3634aaff84bdfb3ae0dcb
ts00 <gts00250@gmail.com.com>
2026-01-09 17:11:11 -0300

    helper functions for more complex systems

--- classics/array_quad_class.py
+++ classics/array_quad_class.py
@@ -5,7 +5,7 @@ class Node:
         self.pos = [0,0]
         self.basket = []
         self.size = size
-        self.children = [0,0,0,0]
+        self.children = []
         self.amount = 0
         self.color = (0,0,0)
         self.level = 0
@@ -14,10 +14,41 @@ class Node:
     
     def clear(self):
         self.__init__()
-            
-    def add_value(self,value):
+
+    def inside(self,value):
         if ((value.pos[0] >= self.pos[0] and value.pos[0] <= self.pos[0] + self.size) and
         (value.pos[1] >= self.pos[1] and value.pos[1] <= self.pos[1] + self.size)):
+            return True
+        
+    def subdiv(self):
+        new_child1 = Node()
+        new_child1.pos = [self.pos[0],self.pos[1]]
+        new_child1.size  = self.size/2
+        new_child1.level = self.level+1
+
+        new_child2 = Node()
+        new_child2.pos = [self.pos[0],self.pos[1]+self.size/2]
+        new_child2.size  = self.size/2
+        new_child2.level = self.level+1
+
+        new_child3 = Node()
+        new_child3.pos = [self.pos[0]+self.size/2,self.pos[1]]
+        new_child3.size  = self.size/2
+        new_child3.level = self.level+1
+
+        new_child4 = Node()
+        new_child4.pos = [self.pos[0] + self.size/2,self.pos[1] + self.size/2]
+        new_child4.size  = self.size/2
+        new_child4.level = self.level+1
+
+        self.children = [new_child1, new_child2, new_child3, new_child4]
+
+
+        
+
+        
+    def add_value(self,value):
+        if self.inside(value):
             self.amount += 1
             self.t += value.t
             if len(self.basket) < self.basket_size or self.size < 2:
@@ -26,50 +57,29 @@ class Node:
                 if value.pos[0] < self.pos[0] + self.size/2:
                     if value.pos[1] < self.pos[1] + self.size/2:
                         
-                        if self.children[0] != 0:
+                        if self.children:
                             self.children[0].add_value(value)
                         else:
-                            new_child = Node()
-                            new_child.pos = [self.pos[0],self.pos[1]]
-                            new_child.size  = self.size/2
-                            new_child.level = self.level+1
-                            new_child.add_value(value)
-                            
-                            self.children[0] = new_child
+                            self.subdiv()
+                            self.children[0].add_value(value)
                     else:
-                        if self.children[1] != 0:
+                        if self.children:
                             self.children[1].add_value(value)
                         else:
-                            new_child = Node()
-                            new_child.pos = [self.pos[0],self.pos[1]+self.size/2]
-                            new_child.size  = self.size/2
-                            new_child.level = self.level+1
-                            new_child.add_value(value)
-                            
-                            self.children[1] = new_child
+                            self.subdiv()
+                            self.children[1].add_value(value)
                             
                 else:
                     if value.pos[1] < self.pos[1] + self.size/2:
-                        if self.children[2] != 0:
+                        if self.children:
                             self.children[2].add_value(value)
                         else:
-                            new_child = Node()
-                            new_child.pos = [self.pos[0]+self.size/2,self.pos[1]]
-                            new_child.size  = self.size/2
-                            new_child.level = self.level+1
-                            new_child.add_value(value)
-
-                            self.children[2] = new_child
+                            self.subdiv()
+                            self.children[2].add_value(value)
+                            
                     else:
-                        if self.children[3] != 0:
+                        if self.children:
                             self.children[3].add_value(value)
                         else:
-                            new_child = Node()
-                            new_child.pos = [self.pos[0] + self.size/2,self.pos[1] + self.size/2]
-                            new_child.size  = self.size/2
-                            new_child.level = self.level+1
-                            new_child.add_value(value)
-
-                            self.children[3] = new_child
-        else:
-            value.update()
+                            self.subdiv()
+                            self.children[3].add_value(value)
