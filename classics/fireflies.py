5861d49d7d03612b825049713c512644b55584a7
ts00 <gts00250@gmail.com.com>
2026-01-25 12:51:22 -0300

    fixed quadtree search

--- classics/fireflies.py
+++ classics/fireflies.py
@@ -1,15 +1,20 @@
-from math import sqrt
+from math import dist,sqrt
+from graph1d import Graph
 import pygame
 from random import randint
-screensize = [800,400]
+screensize = [400,400]
 screen = pygame.display.set_mode((screensize[0],screensize[1]))
+from array_quad_class import Node
+
+
 class fly:
     def __init__(self):
         self.pos = [randint(0,screensize[0]),randint(0,screensize[1])]
         self.speed = [randint(-10,10)/10,randint(-10,10)/10]
-        self.size = 10
+        self.size = 2
         self.peak = 10
         self.trasfer = 0
+        self.t = 0
         self.glowing = randint(0,1)
         self.charge = randint(0,100)/10
     
@@ -27,6 +32,7 @@ class fly:
             self.glowing = 1
         if self.charge <= 0:
             self.glowing = 0
+
         if self.pos[0] > screensize[0]-self.size:
             self.pos[0] = screensize[0]-self.size
             self.speed[0] *= -1
@@ -43,120 +49,32 @@ class fly:
             self.pos[1] = 0
             self.speed[1] *= -1
         
-        if self.charge < 0:
-            self.charge = 0
-       
+        
         g = 255-(self.charge*25)
+
         if g < 0:
             g = 0
         elif g > 255:
             g = 255
         color = (g,g/2,0)
+        if not self.glowing:
+            g = 0
         
        
-        pygame.draw.circle(screen,color,(self.pos[0],self.pos[1]),5)
-        pygame.draw.circle(screen,(50,50,50),(self.pos[0],self.pos[1]),5,1)
+        pygame.draw.circle(screen,color,(self.pos[0],self.pos[1]),self.size)
+        pygame.draw.circle(screen,(50,50,50),(self.pos[0],self.pos[1]),self.size,1)
         
         
     def interact(self,partner):
-            if abs(self.pos[1]-partner.pos[1]) < 20:
-                if partner.charge < self.charge:
-                    partner.charge += 1
-                else:partner.charge -= 1
-class Graph:
-    def __init__(self):
-        self.data = []
-        self.data_lenght = 100
-    
-    def insert(self,point):
-        if len(self.data) > self.data_lenght:
-            self.data.remove(self.data[0])
-        self.data.append(point)
-    
-    def background(self):
-        rect = (5,screensize[1]-50,110,50)
-        pygame.draw.rect(screen,"black",rect)
-        pygame.draw.rect(screen,"white",rect,1)
-        
-    def show(self,color):
-        x = 0
-        for p in self.data:
-            pygame.draw.circle(screen,color,((x)+10,screensize[1]-(p*10)),1)
-            x += 1
-        
-class Node:
-    def __init__(self):
-        self.midle = screensize[0]/2
-        self.width = screensize[0]/2
-        self.basket = []
-        self.basket_size  = 4
-        self.lc = None
-        self.hc = None
-    
-    def holds(self,item):
-        
-        if item.pos[0] > self.midle - self.width:
-            if item.pos[0] < self.midle + self.width:
-                return True
-            else:return False
-        else:return False
-    
-    def add(self,item):
-        if self.holds(item):
-            if len(self.basket) < self.basket_size or self.width <= 2:
-                self.basket.append(item)
-            elif item.pos[0] < self.midle:
-                if self.lc == None:
-                    self.lc = Node()
-                    self.lc.midle = self.midle-(self.width/2)
-                    self.lc.width = self.width/2
-                    self.lc.basket_size = self.basket_size
-                    self.lc.add(item)
-                else:
-                    self.lc.add(item)
-            
-            else:
-                if self.hc == None:
-                    self.hc = Node()
-                    self.hc.midle = self.midle+(self.width/2)
-                    self.hc.width = self.width/2
-                    self.hc.basket_size = self.basket_size
-                    self.hc.add(item)
-                else:
-                    self.hc.add(item)
-        else:
-            item.update()
-    
-    def update(self):
-        for i in self.basket:
-            i.update()
-        if self.lc != None:
-            self.lc.update()
-        if self.hc != None:
-            self.hc.update()
-            
-    def vizualize(self):
-        pygame.draw.line(screen,"white",(self.midle,0),(self.midle,screensize[1]))
-        
-        if self.lc != None:
-            self.lc.vizualize()
-            
-        if self.hc != None:
-            self.hc.vizualize()
+            if dist(self.pos,partner.pos) < 10 and partner != self:
+                partner.charge += (self.charge-partner.charge)*0.1
+  
+
 
 clock = pygame.time.Clock()
 level = []
 
-def deep_interact(fly,node):
-    if node.holds(fly):
-        for i in node.basket:
-            if i != fly:
-                fly.interact(i)
-            
-        if node.lc!= None:
-            deep_interact(fly,node.lc)
-        if node.hc!= None:
-            deep_interact(fly,node.hc)
+
 
 def average_charge(array):
     x = 0
@@ -172,15 +90,20 @@ def deviation(array,average = None):
     variance = 0
     for i in array:
         variance += (i.charge-av)**2
-    variance/=len(array)
+    
     return sqrt(variance)
     
 
-for i in range(500):
+for i in range(400):
     level.append(fly())
 
-data1 = Graph()
-data2 = Graph()
+data1 = Graph(screensize,screen)
+data2 = Graph(screensize,screen)
+
+def interact(obj1,obj2):
+    obj1.interact(obj2)
+    obj2.interact(obj1)
+
 
 while True:
     for event in pygame.event.get():
@@ -188,20 +111,23 @@ while True:
             pygame.quit()
     screen.fill("black")
     
-    node = Node()
+    node = Node(400)
     for i in level:
-        node.add(i)
+        node.add_value(i)
         i.update()
     for i in level: 
-        deep_interact(i,node)
+        node.apply_action(i,interact)
+
     var = average_charge(level)/2
     data1.insert(var)
     
     data2.insert(deviation(level,var))
     data1.background()
+    
+    
+    #node.show_self(100,screen)
     data1.show("blue")
     data2.show("Red")
-    
     #print(round(var,2))
     clock.tick(60)
     pygame.display.flip()
