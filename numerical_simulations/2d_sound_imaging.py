415614a934efece7825da4d12a0a3bc41addf858
ts00 <gts00250@gmail.com.com>
2026-01-25 12:43:55 -0300

    minor improvements

--- numerical_simulations/2d_sound_imaging.py
+++ numerical_simulations/2d_sound_imaging.py
@@ -3,7 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import os
 
+
+# // helper function for object placement //
 def place_sphere(pos, radius, field, value):
     x = np.arange(field.shape[0]) - pos[0]
     y = np.arange(field.shape[1]) - pos[1]
@@ -14,21 +17,53 @@ def place_sphere(pos, radius, field, value):
     field[mask] = value
     return field
 
+def place_box(pos, side, field, value):
+    field[pos[0]-side:pos[0]+side,pos[1]-side:pos[1]+side] = value
+    return field
 
 
-scene = " disk at (100,100), radius = 50"
 
-line = np.linspace(0,1,200)*40*0.444
+
+
+
+# // field oscilation functions 
 def pulse(t,t0 = 0,s = 1):
     return 1/np.exp(((t-t0)/s)**2)
 
-def oscilate(t,t0,f,a):
-    return np.sin((t-t0)*f)*a
+def oscilate(t,t0,a):
+    return np.sin(freq*(t-t0))*a
 
 
 def step(t,t0,w,a):
     return (abs(t-t0)<w)*a
 
+
+
+# boundary conditions
+def boundary2d(field,t):
+    match t:
+        case 0:
+            field[0,:] = 0
+            field[-1,:] = 0
+
+            field[:,0] = 0
+            field[:,-1] = 0
+            
+        case 1:
+            field[0,:] = field[1,:]
+            field[-1,:] = field[-2,:]
+
+            field[:,0] = field[:,1]
+            field[:,-1] = field[:,-2]
+        
+        case 2:
+            field[0,:] = field[1,:] - (field[1,:]-field[2,:])
+            field[-1,:] = field[-2,:] - (field[-2,:]-field[-3,:])
+
+            field[:,0] = field[:,1] - (field[:,1]-field[:,2])
+            field[:,-1] = field[:,-2] - (field[:,-2]-field[:,-3])
+            
+    return field
 
 
 #h0[:,1] = y
@@ -46,17 +81,22 @@ def run(n,sub,viz = 1):
     hit = False
     
 
-    #h0 = np.ones_like(grid)*0
-    #h1 = np.ones_like(grid)*-0
-
+    h0 = np.ones_like(grid)*0
+    h1 = np.ones_like(grid)*-0
+    time = []
     for i in range(n):
 
         
-        #h0[:,0] = np.sin(np.linspace(0,100,100) + i/2)
-        #h1[:,0] = np.sin(np.linspace(0,100,100) + i/2)
+        if  mesure_start-1 < i < mesure_stop:
+            time.append((
 
-        if i > mesure_start-1:
-            time.append(np.abs(h0[90:110,1]).sum())
+                np.abs(h0[sensor[0]:sensor[1],sensor[2]]).sum()
+                )/(sensor[1]-sensor[0])
+                
+                )
+        elif i >= mesure_stop:
+            break
+            
           
         
         
@@ -65,8 +105,8 @@ def run(n,sub,viz = 1):
            
 
             if i < emit_stop:
-                h0[80:120,1] = oscilate(i*sub + j,shift,freq,1)[80:120]
-                h1[80:120,1] = h0[80:120,1]
+                h0[emissor[0]:emissor[1],emissor[2]] = oscilate((i*sub + j)*dt,shift,1)
+                h1[emissor[0]:emissor[1],emissor[2]] = h0[emissor[0]:emissor[1],emissor[2]]
             
 
             dh = np.zeros_like(h0)
@@ -79,24 +119,15 @@ def run(n,sub,viz = 1):
                 
         
 
-            h2 = (2*h1 - h0 ) + dh*(dt*dt)*(c*c) - ((h1-h0)/dt)*0.0001
+            h2 = (2*h1 - h0 ) + dh*(dt*dt)*(c*c) - ((h1-h0)/dt)*0.001
 
             
             h0 = h1
             h1 = h2
 
-         
-            h0[0,:] = 0
-            h0[-1,:] = 0
+            h0 = boundary2d(h0,0)
+            h1 = boundary2d(h1,0)
 
-            h0[:,0] = 0
-            h0[:,-1] = 0 
-
-            h1[0,:] = 0
-            h1[-1,:] = 0
-
-            h1[:,0] = 0
-            h1[:,-1] = 0 
 
             
 
@@ -104,9 +135,9 @@ def run(n,sub,viz = 1):
         def plot():
            
            
-            plt.title(f"i = {i}")
             ax_sim.cla()
-            ax_sim.imshow(np.abs(h1),vmax = 1,vmin = 0)
+            ax_sim.set_title(f"i = {i}")
+            ax_sim.imshow(np.abs(h0),vmax = max_light,vmin = 0)
             plt.pause(0.001)
             
             
@@ -114,36 +145,70 @@ def run(n,sub,viz = 1):
 
         #plot()
         if i % 10 == 1 and viz:
+             
             plot()
+    return time
 
  
 
+
+# // general constants //  
     
 data = []  
-
 
 dt = 0.1
 widt_height = [200,200]
 grid = np.zeros(widt_height)
 c = np.ones_like(grid)*7
-freq = 0.7
+c = boundary2d(c,0)
+f = 1
+freq = (2*np.pi)*f
 
 
-rand_init = np.random.random(widt_height)*0.01
+rand_init = np.random.random(widt_height)*0.0
 h0 = rand_init
 h1 = rand_init
 
-angle_range = 60
-angle_res = 20
-steps = 100
-substeps = 10
+angle_range = 30
+angle_res = 30
+steps = 600
+substeps = 1
 emit_stop = 10
 mesure_start = 10
-max_light = 5
+mesure_stop = steps
+max_light = 0.1
+sensor = [90,110,2]
+emissor = [90,110,1]
+
+emissorMean = (emissor[1]+emissor[0])/2
+emissormax = emissor[1]-emissorMean
+dp = np.linspace(-emissormax,emissormax,emissor[1]-emissor[0])
 
 
-c = place_sphere([100,100],20,c,0)
-#c[100:150,75:125] = 0
+
+
+n_disks = 0
+for i in range(0):
+    n_disks += 1
+    c = place_box(
+        [random.randint(20,180),
+         random.randint(20,180)],
+         10,c,0
+        )
+
+for i in range(10):
+    n_disks += 1
+    c = place_sphere(
+        [random.randint(20,180),
+         random.randint(20,180)],
+         10,c,0
+        )
+
+
+
+scene = f"{n_disks} disks scattered around the box"
+
+
 
 
 
@@ -157,46 +222,64 @@ plt.tight_layout()
 
 
 
+# /// test_run /// 
 
-for i  in np.linspace(-angle_range,angle_range,2*angle_res):
-    time = []
+
+shift = dp*np.deg2rad(-30)/c.mean()
+#time = run(700,1,1) 
+    
+
+t = 0 
+
+# /// simulation loop /// 
+for i  in np.linspace(-angle_range,angle_range,angle_res):
+    
+    
     #brute forced magical values
-    shift = np.linspace(0,200,200)*(i)*0.02
-    print(
-        round(
-            100*(
-                (i-(-angle_range))/
-                (angle_range-(-angle_range))),
-            2
-            )
-         
-    )
+
+    os.system("cls")
+    shift = dp*np.deg2rad(-i)/c.mean()
+    percent = round(100*((i-(-angle_range))/(angle_range-(-angle_range))))
+    print((percent//10)*"I",percent,"%")
+    
+    #simulation
+    
+    time = run(steps,substeps,0) 
     
     
-    run(steps,substeps,1)
     data.append(time)
 
 
+
+data = np.array(data)
+
 ax_sim.cla()
 
-ax_sim.imshow(data,vmax = max_light, aspect='auto')
+ax_sim.imshow(c,vmax = c.mean(), aspect='auto')
 ax_sim.set_title(f"ultra sound data of {scene}")
-plt.pause(0.01)
+plt.pause(0.1)
 
-#ax.cla()
+
 theta = np.deg2rad(
-    np.linspace(-angle_range, angle_range, 2*angle_res)
+    np.linspace(-angle_range, angle_range, angle_res)
 )
-radius = np.linspace(mesure_start,steps,steps-mesure_start)
+radius = np.linspace(
+    mesure_start*substeps*c.mean()*dt,
+    mesure_stop*substeps*c.mean()*dt,
+    mesure_stop-mesure_start
+    )
 
 Radius,Theta = np.meshgrid(radius,theta)
 
 fig, ax_pol = plt.subplots(1,1,subplot_kw={'projection': 'polar'})
+ax_pol.xaxis.grid(False)   # angle lines OFF
+ax_pol.yaxis.grid(False)   # angle lines OFF
 ax_pol.set_thetamin(-angle_range)
 ax_pol.set_thetamax(angle_range)
 ax_pol.set_title(f"projected ultra sound data of {scene}")
 
-ax_pol.pcolormesh(theta,radius,np.array(data).T,vmax = max_light)
+ax_pol.pcolormesh(theta,radius,np.array(data).T,vmax = max_light,cmap="hot")
+
 #ax.imshow(data,vmax=1,vmin=-0.0)
 #plt.plot(np.linspace(0,len(time)*dt,len(time)),time)
 
