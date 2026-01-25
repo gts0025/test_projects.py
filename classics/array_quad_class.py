efd63bdd2668abd35c38b4c057ca7dfb1ab7271e
ts00 <gts00250@gmail.com.com>
2026-01-25 12:49:04 -0300

    added vizualization, and  Refactor tree traversal to use visitor-style action callback

--- classics/array_quad_class.py
+++ classics/array_quad_class.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame,random
+
 class Node:
     def __init__(self,size = 700,basket_size = 4) -> None:
     
@@ -15,7 +16,7 @@ class Node:
     def clear(self):
         self.__init__()
 
-    def inside(self,value):
+    def enclose(self,value):
         if ((value.pos[0] >= self.pos[0] and value.pos[0] <= self.pos[0] + self.size) and
         (value.pos[1] >= self.pos[1] and value.pos[1] <= self.pos[1] + self.size)):
             return True
@@ -43,17 +44,28 @@ class Node:
 
         self.children = [new_child1, new_child2, new_child3, new_child4]
 
-
-        
-
+    
+    def apply_action(self,obj,action):
+        if self.enclose(obj):
+            for target in self.basket:
+                if target != obj:
+                    action(obj,target)
+            for i in self.children:
+                i.apply_action(obj,action)
         
     def add_value(self,value):
-        if self.inside(value):
+        if self.enclose(value):
             self.amount += 1
             self.t += value.t
-            if len(self.basket) < self.basket_size or self.size < 2:
+            if len(self.basket) < self.basket_size and (not self.children or self.size <= 2.5):
                 self.basket.append(value)
+                
             else:
+                if self.basket and self.children:
+                    for i in self.basket:
+                        self.basket.remove(i)
+                        self.add_value(i)
+
                 if value.pos[0] < self.pos[0] + self.size/2:
                     if value.pos[1] < self.pos[1] + self.size/2:
                         
@@ -83,3 +95,11 @@ class Node:
                         else:
                             self.subdiv()
                             self.children[3].add_value(value)
+
+    def show_self(self,seed,screen):
+        random.seed(self.level + seed)
+        color = [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+        pygame.draw.rect(screen,color,[self.pos[0],self.pos[1],self.size,self.size],1)
+        if self.children:
+            for i in self.children:
+                i.show_self(seed,screen)
