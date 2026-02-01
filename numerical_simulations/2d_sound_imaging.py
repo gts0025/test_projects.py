4db42e6e56378d3c2b3f670ee661209ffbf22d6e
ts00 <gts00250@gmail.com.com>
2026-02-01 09:22:01 -0300

    applying field tools and minor fixes

--- numerical_simulations/2d_sound_imaging.py
+++ numerical_simulations/2d_sound_imaging.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import os
+from fieldTools import second_derivative,fullBoundary2d
 
 
 # // helper function for object placement //
@@ -21,8 +22,25 @@ def place_box(pos, side, field, value):
     field[pos[0]-side:pos[0]+side,pos[1]-side:pos[1]+side] = value
     return field
 
+def random_field(c):
+    noise = np.ones_like(c)
+    noise[1:-1,1:-1] = np.random.random(c[1:-1,1:-1].shape)
 
+    noise[:,:5] = 1
+    for i in range(100):
+        noise[1:-1,1:-1] += (
 
+            noise[1:-1,2:] +
+            noise[1:-1,:-2] + 
+
+            noise[2:,1:-1] +
+            noise[:-2,1:-1] -
+
+            4*noise[1:-1,1:-1]
+            )*0.1
+        
+    c[noise < 0.49] = 0
+    return c
 
 
 
@@ -38,32 +56,6 @@ def step(t,t0,w,a):
     return (abs(t-t0)<w)*a
 
 
-
-# boundary conditions
-def boundary2d(field,t):
-    match t:
-        case 0:
-            field[0,:] = 0
-            field[-1,:] = 0
-
-            field[:,0] = 0
-            field[:,-1] = 0
-            
-        case 1:
-            field[0,:] = field[1,:]
-            field[-1,:] = field[-2,:]
-
-            field[:,0] = field[:,1]
-            field[:,-1] = field[:,-2]
-        
-        case 2:
-            field[0,:] = field[1,:] - (field[1,:]-field[2,:])
-            field[-1,:] = field[-2,:] - (field[-2,:]-field[-3,:])
-
-            field[:,0] = field[:,1] - (field[:,1]-field[:,2])
-            field[:,-1] = field[:,-2] - (field[:,-2]-field[:,-3])
-            
-    return field
 
 
 #h0[:,1] = y
@@ -87,19 +79,10 @@ def run(n,sub,viz = 1):
     for i in range(n):
 
         
-        if  mesure_start-1 < i < mesure_stop:
-            time.append((
-
-                np.abs(h0[sensor[0]:sensor[1],sensor[2]]).sum()
-                )/(sensor[1]-sensor[0])
-                
-                )
-        elif i >= mesure_stop:
+      
+        if i >= mesure_stop:
             break
-            
-          
-        
-        
+       
         
         for j in range(sub):
            
@@ -108,25 +91,28 @@ def run(n,sub,viz = 1):
                 h0[emissor[0]:emissor[1],emissor[2]] = oscilate((i*sub + j)*dt,shift,1)
                 h1[emissor[0]:emissor[1],emissor[2]] = h0[emissor[0]:emissor[1],emissor[2]]
             
+            if  mesure_start-1 < i < mesure_stop:
+                time.append((
 
-            dh = np.zeros_like(h0)
-            dh[1:-1,1:-1] += (
-                ( h1[2:,1:-1] + h1[:-2,1:-1] - 2*h1[1:-1,1:-1] )+
-                ( h1[1:-1,2:] + h1[1:-1,:-2] - 2*h1[1:-1,1:-1] )
-            )
+                    np.abs(h0[sensor[0]:sensor[1],sensor[2]]).sum()
+                    )/(sensor[1]-sensor[0])
+                    
+                    )
 
-            
-                
-        
+            dh = (
+                second_derivative(h1,0,1)+
+                second_derivative(h1,1,1)
+                )
 
-            h2 = (2*h1 - h0 ) + dh*(dt*dt)*(c*c) - ((h1-h0)/dt)*0.001
+
+            h2 = (2*h1 - h0 ) + dh*(dt*dt)*(c*c) - ((h1-h0))*u
 
             
             h0 = h1
             h1 = h2
 
-            h0 = boundary2d(h0,0)
-            h1 = boundary2d(h1,0)
+            h0 = fullBoundary2d(h0,0)
+            h1 = fullBoundary2d(h1,0)
 
 
             
@@ -137,7 +123,7 @@ def run(n,sub,viz = 1):
            
             ax_sim.cla()
             ax_sim.set_title(f"i = {i}")
-            ax_sim.imshow(np.abs(h0),vmax = max_light,vmin = 0)
+            ax_sim.imshow(np.abs(h0),vmax = sim_max,vmin = 0)
             plt.pause(0.001)
             
             
@@ -157,28 +143,30 @@ def run(n,sub,viz = 1):
 data = []  
 
 dt = 0.1
-widt_height = [200,200]
+widt_height = [100,100]
 grid = np.zeros(widt_height)
-c = np.ones_like(grid)*7
-c = boundary2d(c,0)
-f = 1
+c = np.ones_like(grid)*6
+c = fullBoundary2d(c,0)
+f = 2
 freq = (2*np.pi)*f
 
 
-rand_init = np.random.random(widt_height)*0.0
-h0 = rand_init
-h1 = rand_init
+c += np.random.random(widt_height)*0.1
+h0 = grid
+h1 = grid
 
 angle_range = 30
-angle_res = 30
+angle_res = 60
 steps = 600
+u = 0.01
 substeps = 1
-emit_stop = 10
+emit_stop = 5
 mesure_start = 10
 mesure_stop = steps
-max_light = 0.1
-sensor = [90,110,2]
-emissor = [90,110,1]
+viz_max = 0.1
+sim_max = 0.1
+sensor = [45,55,2]
+emissor = [45,55,1]
 
 emissorMean = (emissor[1]+emissor[0])/2
 emissormax = emissor[1]-emissorMean
@@ -196,15 +184,15 @@ for i in range(0):
          10,c,0
         )
 
-for i in range(10):
+for i in range(0):
     n_disks += 1
     c = place_sphere(
-        [random.randint(20,180),
-         random.randint(20,180)],
+        [random.randint(20,80),
+         random.randint(20,80)],
          10,c,0
         )
 
-
+c = random_field(c)
 
 scene = f"{n_disks} disks scattered around the box"
 
@@ -226,7 +214,7 @@ plt.tight_layout()
 
 
 shift = dp*np.deg2rad(-30)/c.mean()
-#time = run(700,1,1) 
+time = run(steps,1,1) 
     
 
 t = 0 
@@ -264,9 +252,9 @@ theta = np.deg2rad(
     np.linspace(-angle_range, angle_range, angle_res)
 )
 radius = np.linspace(
-    mesure_start*substeps*c.mean()*dt,
-    mesure_stop*substeps*c.mean()*dt,
-    mesure_stop-mesure_start
+    mesure_start*substeps,
+    mesure_stop*substeps,
+    (mesure_stop-mesure_start)*substeps
     )
 
 Radius,Theta = np.meshgrid(radius,theta)
@@ -278,7 +266,7 @@ ax_pol.set_thetamin(-angle_range)
 ax_pol.set_thetamax(angle_range)
 ax_pol.set_title(f"projected ultra sound data of {scene}")
 
-ax_pol.pcolormesh(theta,radius,np.array(data).T,vmax = max_light,cmap="hot")
+ax_pol.pcolormesh(theta,radius,np.array(data).T,vmax = viz_max,cmap="hot")
 
 #ax.imshow(data,vmax=1,vmin=-0.0)
 #plt.plot(np.linspace(0,len(time)*dt,len(time)),time)
