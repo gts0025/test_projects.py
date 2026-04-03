3d0e82a632505a03e18fde1d4ea5a8a2df675e11
ts00 <gts00250@gmail.com.com>
2026-04-02 22:45:03 -0300

    applying kutta 2

--- numerical_simulations/2d_shrodiger.py
+++ numerical_simulations/2d_shrodiger.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 plank = 0.01
 i = 1j
 m = 1
-dt = 0.01
+dt = 0.7
 size  = 20
 dx = 0.3
 cells = round(size/dx)
@@ -14,28 +14,45 @@ a_x,a_y = np.meshgrid(x_line,y_line)
 
 wave = np.zeros(shape=[cells,cells],dtype=complex)
 dist  = np.sqrt(a_x**2 + a_y**2)
-gaussian = 1/np.exp(a_x**2 + a_y**2) 
+gaussian = np.exp(-(a_x**2 + a_y**2)) 
 wave.imag = (gaussian)
 
 
 
-        
+def laplace(wave):
+    d2ux = (wave[2:,1:-1] + wave[:-2,1:-1] - 2*wave[1:-1,1:-1])/(dx**2)
+    d2uy = (wave[1:-1,2:] + wave[1:-1,:-2] - 2*wave[1:-1,1:-1])/(dx**2)
+    zero =  np.zeros_like(wave)
+    zero[1:-1,1:-1] +=  (d2ux + d2uy)
+
+    return zero
+
+
 def show(steps,substeps):
     p = []
+    global wave
     for step in range(steps):
 
         plt.title(f"shrodiger equation: time:{round(step*substeps*dt*plank)}s")
-        plt.imshow(abs(wave)**2, cmap="jet")
+       
+        plt.imshow(abs(wave), cmap="inferno", vmax= 1, vmin = 0 )
+        plt.colorbar()
+        
         p.append((abs(wave)**2).mean())
         #plt.plot(p)
-        plt.colorbar()
+       
         plt.pause(0.0001)
         plt.clf()
 
         for substep in range(substeps):
-            d2ux = (wave[2:,1:-1] + wave[:-2,1:-1] - 2*wave[1:-1,1:-1])/(dx**2)
-            d2uy = (wave[1:-1,2:] + wave[1:-1,:-2] - 2*wave[1:-1,1:-1])/(dx**2)
-            wave[1:-1,1:-1] += ( ( -( d2ux+d2uy )*( plank**2/(2*m) ) ) / (plank*i) )*dt
+
+            d2ux = laplace(wave) 
+            k1 = ( ( -( d2ux )*( plank**2/(2*m) ) ) / (plank*i) )
+            w2 = wave + k1*dt
+            
+            d2ux = laplace(w2) 
+            k2 = ( ( -( d2ux )*( plank**2/(2*m) ) ) / (plank*i) )
+            wave += dt*(k1 + k2)/2
 
         
 
