5760c2ea49435e23bd07304071d47369dea377b9
ts00 <gts00250@gmail.com.com>
2026-04-07 19:41:40 -0300

    implemented RK4 on the 2d shrodinger equation

--- numerical_simulations/2d_shrodiger.py
+++ numerical_simulations/2d_shrodiger.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 plank = 0.01
 i = 1j
 m = 1
-dt = 0.7
-size  = 20
-dx = 0.3
-cells = round(size/dx)
+dt = 10
+cells = 100
+pulse_size  = 10
+dx = 1
 
-x_line = np.linspace(-size/2,size/2,cells)
-y_line = np.linspace(-size/2,size/2,cells)
-a_x,a_y = np.meshgrid(x_line,y_line)
+
+x_line = np.linspace(-pulse_size/2,pulse_size/2,cells)
+y_line = x_line.copy()
+xl,yl = np.meshgrid(x_line,y_line)
 
 wave = np.zeros(shape=[cells,cells],dtype=complex)
-dist  = np.sqrt(a_x**2 + a_y**2)
-gaussian = np.exp(-(a_x**2 + a_y**2)) 
+
+gaussian = np.exp(-(xl**2 + yl**2)) 
 wave.imag = (gaussian)
 
 
@@ -33,9 +34,8 @@ def show(steps,substeps):
     global wave
     for step in range(steps):
 
-        plt.title(f"shrodiger equation: time:{round(step*substeps*dt*plank)}s")
-       
-        plt.imshow(abs(wave), cmap="inferno", vmax= 1, vmin = 0 )
+        plt.title(f"shrodiger equation: time: {round(step*substeps)} steps")
+        plt.imshow(abs(wave), cmap="inferno" )
         plt.colorbar()
         
         p.append((abs(wave)**2).mean())
@@ -46,13 +46,20 @@ def show(steps,substeps):
 
         for substep in range(substeps):
 
-            d2ux = laplace(wave) 
-            k1 = ( ( -( d2ux )*( plank**2/(2*m) ) ) / (plank*i) )
-            w2 = wave + k1*dt
             
-            d2ux = laplace(w2) 
-            k2 = ( ( -( d2ux )*( plank**2/(2*m) ) ) / (plank*i) )
-            wave += dt*(k1 + k2)/2
+            time_constant = ( plank**2/(2*m) )  / (plank*i) 
+            k1 = (laplace(wave))
+            
+            w2 = wave + time_constant*k1*dt/2
+            k2 = (laplace(w2))
+            
+            w3 = wave + time_constant*k2*dt/2
+            k3 = (laplace(w3))
+            
+            w4 = wave + time_constant*k3*dt
+            k4 = (laplace(w4))
+
+            wave += dt*time_constant*(k1 + 2*k2 + 2*k3 + k4)/6
 
         
 
