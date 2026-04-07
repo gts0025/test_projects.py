219ee6892889366fb43d66f14bcbf2f73653eaeb
ts00 <gts00250@gmail.com.com>
2026-04-07 19:31:25 -0300

    improving the numerical method, and adding non uniform potential

--- numerical_simulations/1d gas equation.py
+++ numerical_simulations/1d gas equation.py
@@ -10,17 +10,17 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # p = kh
 
 plt.winter()
-u = np.zeros(200)
+u = np.ones(200)*0
 
 x = np.linspace(0,1,u.shape[0]) 
 y = abs(x-0.5)**2 + np.random.random(x.shape[0])*0.01
-h = np.ones_like(u)*0.5
+h = np.ones_like(u)*0.1
 b = y
 
 
 
-h[90:110] += 0.4
-#h[20:30] += 0.4
+#h[:90] += 0.4
+h[180:] += np.exp(-np.linspace(-2,2,20)**2)
 
 volume = h.sum()
 
@@ -43,8 +43,7 @@ def kutta_step(substeps):
 
     global u,h,k,dt,d,volume
     #boundary cpndition
-    h[0] = h[1]
-    h[-1] = h[-2]
+    
     #u[0] = u[1]
     #u[-1] = u[-2]
     
@@ -54,28 +53,53 @@ def kutta_step(substeps):
     #solve
     for i in range(substeps):
         p = h**k
+
         dh1 = -(derivative(u*p) - second_derivative(h)*d)
         du1 = -(derivative(p)/h - second_derivative(u)*d)
         
+        nh1 = h + dh1*dt/2
+        nu1 = u + du1*dt/2
+        np1 = (nh1)**k
         
-        nh = h + dh1*dt
-        nu = u + du1*dt
-        np = (nh)**k
-        
-        dh2 = -(derivative((nu)*np) - second_derivative(nh)*d)
-        du2 = -(derivative(np)/(nh) - second_derivative(nu)*d)
+        dh2 = -(derivative((nu1)*np1) - second_derivative(nh1)*d)
+        du2 = -(derivative(np1)/(nh1) - second_derivative(nu1)*d)
 
-        u += (dt)*(du1+du2)/2
-        h += (dt)*(dh1+dh2)/2
+        nh2 = h + dh2*dt/2
+        nu2 = u + du2*dt/2
+        np2 = (nh2)**k
+        
+        dh3 = -(derivative((nu2)*np2) - second_derivative(nh2)*d)
+        du3 = -(derivative(np2)/(nh2) - second_derivative(nu2)*d)
+
+        nh3 = h + dh3*dt
+        nu3 = u + du3*dt
+        np3 = (nh3)**k
+        
+        dh4 = -(derivative((nu3)*np3) - second_derivative(nh3)*d)
+        du4 = -(derivative(np3)/(nh3) - second_derivative(nu3)*d)
+
+       
+
+        u += (dt)*(du1+ 2*du2 + 2*du3 + du4)/6
+        h += (dt)*(dh1+ 2*dh2 + 2*dh3 + dh4)/6
+        
+        h[0] = 1
+        h[1] = (h[0]  + h[2])/2   # x-1 - 2x + x+1 = 0
+        h[-1] = h[-2]
+        u[0] = 20
+        u[-1] = u[-2]
+
+      
+
+        
 
 def euler_step(substeps):
 
     global u,h,k,dt,d,volume
     #boundary cpndition
-    h[0] = h[1]
+    h[0] = 1
+    h[1] = (h[0]  + h[2])/2   # x-1 - 2x + x+1 = 0
     h[-1] = h[-2]
-    #u[0] = u[1]
-    #u[-1] = u[-2]
     
     #pressure
     p = h**k
@@ -101,7 +125,7 @@ def solve(steps,substeps):
         plt.title("1d euler gas equation")
         plt.xlabel("distance")
         plt.ylabel("pressure")
-        plt.ylim((0,2))
+        #plt.ylim((-1,1))
         plt.plot(h)
         #plt.plot(u)
         #plt.plot(b)
