a9cffe79bbead4fa049dc58577584f1b83180729
ts00 <gts00250@gmail.com.com>
2026-04-08 20:33:18 -0300

    numerical  solution to the 1/( R ) potential

--- numerical_simulations/1d_shrodiger.py
+++ numerical_simulations/1d_shrodiger.py
@@ -9,24 +9,25 @@ plt.style.use("dark_background")
 
 im = 1j
 
-h = 1.1
-m = 1
+h = 1
+m = 10
 
-dt = 0.01
+dt = 0.1
 dx = 1
-cells = 100
+ke = 10
+cells = 300
 steps = 1000
-substeps = 40
+substeps = 100
 
 
-line = np.linspace(0,np.pi,cells)
+line = np.linspace(-1,1,cells)
+
 wave = np.zeros(shape=[cells],dtype=complex)
-potential = (np.linspace(-1,1,cells)**2) - 0.5
-#potential = np.ones_like(wave.imag)*0
-#wave.imag = np.sin(line)/2
-#wave.real = -np.sin(line)/2
+wave.imag = np.exp(-np.linspace(-2,2,cells)**2)*0.74
+potential = -0.0001/(abs(np.linspace(-1,1,cells)) + 0.01)
 
-wave.imag[20:40] = np.exp(-np.linspace(-2,2,20)**2)
+
+#wave.real[10:30] = np.cos(np.linspace(0,np.pi*ke,20))*np.exp(-np.linspace(-2,2,20)**2)*0.7
 
 
 
@@ -38,7 +39,7 @@ figure,ax = plt.subplots(1,1)
 
 frames = []
 last_p = -1
-
+time_averaged = np.abs(wave)**2
 def laplacian(wave,dx):
     null = np.zeros_like(wave)
     null[1:-1] += (wave[2:] + wave[:-2] - 2*wave[1:-1])/(dx**2)
@@ -47,35 +48,39 @@ def laplacian(wave,dx):
 
 def get_f(wave,h,m,dx,potential):
     d2ux = laplacian(wave,dx)
-    f1 = -(h**2/2*m)*(d2ux)
+    f1 = -(h**2/(2*m))*(d2ux)
     f2 = potential*wave
     return (f1 + f2)/(1j*h)
 
 
 init_p = (np.abs(wave)**2).sum()
 def show(n):
-    global last_p, wave, h, m, dx, potential
+    global last_p, wave, h, m, dx, potential, time_averaged
     plt.cla()
     percent = round((n/steps)*100)
     if (percent != last_p):
         last_p = percent
         current_p = (np.abs(wave)**2).sum()
-        #print(current_p/init_p)
+        print(current_p/init_p)
         
         print(f"running: {percent}%")
     plt.title(f"shrodiger equation")
-    plt.ylim(-1,1)
-    plt.plot(line,abs(wave)**2 + potential, linewidth = 2, label = "probability")
-    plt.plot(line,wave.real + potential,linewidth = 1,  label = "real")
-    plt.plot(line,wave.imag + potential,linewidth = 1, label = "imaginary")
-    plt.plot(line, potential,linewidth = 1, label = "potential")
+    plt.ylim(-2,4)
+    plt.plot(line,abs(wave)**2, linewidth = 2, label = "probability")
+    #plt.plot(line,time_averaged, linewidth = 2, label = "time averaged")
+    #plt.plot(line,wave.real,linewidth = 1,  label = "real")
+    #plt.plot(line,wave.imag,linewidth = 1, label = "imaginary")
+    plt.plot(line, potential/(abs(potential.max()))*0.01,linewidth = 1, label = "potential")
     
     plt.legend(loc="upper right")
     
-    
-    
+    p = np.abs(wave)**2
+    time_averaged = 0.9*time_averaged + 0.1*(p + laplacian(p,dx)*0.5)
+      
     for substep in range(substeps):
         time_constant = ((h**2)/2*m)
+
+      
 
         k1 = get_f(wave,h,m,dx,potential)
 
@@ -108,7 +113,7 @@ if __name__ == "__main__":
     writer = animation.PillowWriter(fps=25,bitrate=400)
     print("running")
     data = animation.FuncAnimation(figure,show, frames = steps, interval = 1)
-    #plt.show()
+    plt.show()
     print("saving")
     data.save(gif_path,writer = writer)
     print("done")
